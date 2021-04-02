f3dda9022b077ca11cc9c8cc5d2ba1395be3c2ed
Bram Cooper <bram.copermans180401@gmail.com>
2021-04-02 20:10:33 +0200

    linking with weather API, logging data from API data

--- Lab4 - app pro/app.js
+++ Lab4 - app pro/app.js
@@ -1,6 +1,41 @@
 class App{
     constructor(){
-        console.log("started!");
+        this.getLocation();
+        this.lat;
+        this.lng;
+    }
+
+    getLocation(){
+        navigator.geolocation.getCurrentPosition(
+            this.gotLocation.bind(this), 
+            this.errorLocation.bind(this)
+            );
+    }
+
+    gotLocation(result){
+        this.lat = result.coords.latitude;
+        this.lng = result.coords.longitude;
+        this.getWeather();
+
+    }
+
+    getWeather(){
+        let url = `https://cors-anywhere.herokuapp.com/http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/${this.lat}/${this.lng}&appid={b435d4c49985b76a1147d0a45dcc22ef}`;
+        fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            document.querySelector("#weather").innerHTML = 
+                data.currently.summary;
+        })
+        .catch(err => {
+            console.log("error");
+        });
+    }
+
+    errorLocation(err){
+        console.log(err);
     }
 }
 
